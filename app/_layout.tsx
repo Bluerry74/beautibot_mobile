4d5d6e06b94b205c572875263234d562430f5cab
phamnguyen1700 <169217913+phamnguyen1700@users.noreply.github.com>
2025-07-09 19:15:26 +0700

    [FIXED] Đã sửa toàn bộ lỗi UI, điều chỉnh lại form đăng ký/ đăng nhập, format để chạy được trên mọi thiết bị - fixed phần lớn lỗi typescript và lint - thêm tanstack, zustand, format lại tree code - Điều chỉnh lại loginSystem - them refresh cho interceptor - thêm store để lưu thay cho context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import Toast from "react-native-toast-message";
+import "../global.css";
+
 
 const queryClient = new QueryClient();
 
