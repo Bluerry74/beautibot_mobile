3ae478804f808a38e45ca47ee1d21754a29f6b44
vipprodemon123 <khoanguyenvan456@gmail.com>
2025-07-18 01:14:58 +0700

    Tracking

--- services/delivery/index.ts
+++ services/delivery/index.ts
@@ -1,5 +1,5 @@
-import { patch } from "@/httpservices/httpService";
-
+import { get, patch } from "@/httpservices/httpService";
+import { IDelivery } from "@/types/order";
 export const uploadProof = async (imageUri: string, deliveryId: string) => {
   const formData = new FormData();
   formData.append("file", {
@@ -21,3 +21,7 @@ export const updateDeliveryStatus = async (deliveryId: string, status: string) =
   const res = await patch(`delivery/${deliveryId}/status`, { status });
   return res.data;
 }
+export const getDeliveriesByCustomer = async (): Promise<IDelivery[]> => {
+  const res = await get("/delivery/customer");
+  return res.data as IDelivery[];
+};
